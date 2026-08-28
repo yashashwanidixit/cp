e5568f41e9e76f72c5ddd130f4d191f66615d019
yashashwani_dixit <yashashwanidixit@gmail.com>
2026-08-29 00:05:52 +0530

    strings

---
+++ strings/some_string_methods.cpp
@@ -0,0 +1,71 @@
+//remove all non alphanumeric characters from a strign
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+const ll MOD = 1000000007;
+const int MAXN = 2000001;
+ll b[MAXN];
+
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+
+void precompute() {
+    b[0] = 1;
+    for (ll i = 1; i < MAXN; i++)
+        b[i] = (i * b[i-1]) % MOD;
+}
+
+ll power(ll n, ll k) {
+    n = n % MOD;
+    ll res = 1;
+    while (k) {
+        if (k % 2) res = res * n % MOD;
+        n = n * n % MOD;
+        k /= 2;
+    }
+    return res;
+}
+
+ll fact(ll n) {
+    return b[n];
+}
+
+ll inverse(ll x) {
+    return power(x, MOD - 2);
+}
+
+ll nCr(ll n, ll r) {
+    if (r < 0 || r > n) return 0;
+    return fact(n) * inverse(fact(r)) % MOD * inverse(fact(n-r)) % MOD;
+}
+
+void solve() {
+    string s  = "iewjfew6747772^7" ;
+    s.erase(remove_if(s.begin(s) , s.end(), [](char c) {
+        return !isalpha(c); 
+    }), s.end()) ;
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    precompute();
+
+    ll t;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
+string s = "uerify74393" ;
+
+
+
+IMPORTANT PATTEN 
+
+s.erase(s.remove_if(a.begin(), s.end(), condition), s.end()) ;
